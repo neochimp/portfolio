ae396c84610e78fbba85969386f0c96d60704a9f
Cameron Lee <7cameronlee@gmail.com>
2025-12-24 02:04:31 -0800

    New Blog: Real Time Linux Scheduler. Updated home page too

--- js/script.js
+++ js/script.js
@@ -1,28 +1,44 @@
 let navOpen = false;
 
 // Closes menu when clicking outside
-window.addEventListener('click', function(e) {
-	const menuBtn = document.getElementById("menuBtn");
-	const mobileMenu = document.getElementById("mobileMenu");
+window.addEventListener("click", function (e) {
+  const menuBtn = document.getElementById("menuBtn");
+  const mobileMenu = document.getElementById("mobileMenu");
 
-	if (!menuBtn.contains(e.target) && !mobileMenu.contains(e.target) && navOpen) {
-		menuIconAnim(menuBtn);
-	}
+  if (
+    !menuBtn.contains(e.target) &&
+    !mobileMenu.contains(e.target) &&
+    navOpen
+  ) {
+    menuIconAnim(menuBtn);
+  }
 });
 
 // Toggle hamburger animation + open/close mobile menu
 function menuIconAnim(x) {
-	x.classList.toggle("change");
-	openNav();
+  x.classList.toggle("change");
+  openNav();
 }
 
 function openNav() {
-	const mobileMenu = document.getElementById("mobileMenu");
-	if (navOpen) {
-		mobileMenu.classList.remove("open");
-		navOpen = false;
-	} else {
-		mobileMenu.classList.add("open");
-		navOpen = true;
-	}
+  const mobileMenu = document.getElementById("mobileMenu");
+  if (navOpen) {
+    mobileMenu.classList.remove("open");
+    navOpen = false;
+  } else {
+    mobileMenu.classList.add("open");
+    navOpen = true;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("a[href]").forEach((link) => {
+    const isExternal =
+      link.hostname && link.hostname !== window.location.hostname;
+
+    if (isExternal) {
+      link.setAttribute("target", "_blank");
+      link.setAttribute("rel", "noopener noreferrer");
+    }
+  });
+});
